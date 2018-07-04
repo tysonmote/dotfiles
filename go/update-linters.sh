f58f1ad30b3854179f95806565d2290954500e40
Tyson Mote <tyson@segment.com>
2018-07-04 16:41:05 -0700

    go: Add mockery to update list

--- go/update-linters.sh
+++ go/update-linters.sh
@@ -4,3 +4,4 @@ go get -u github.com/alecthomas/gometalinter
 gometalinter --install
 go get -u honnef.co/go/tools/cmd/megacheck
 go get -u github.com/nsf/gocode
+go get -u github.com/vektra/mockery/...
