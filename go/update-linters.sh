8357ac4f719b30ed7dedf5afc3ea0037b2984a36
Tyson Mote <tyson@segment.com>
2018-07-16 11:53:39 -0700

    go: Update list of linters to update

--- go/update-linters.sh
+++ go/update-linters.sh
@@ -2,5 +2,23 @@
 
 go get -u github.com/alecthomas/gometalinter
 gometalinter --install
-go get -u honnef.co/go/tools/cmd/megacheck
+
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u github.com/derekparker/delve/cmd/dlv
+go get -u github.com/dominikh/go-tools/cmd/keyify
+go get -u github.com/fatih/gomodifytags
+go get -u github.com/fatih/motion
+go get -u github.com/golang/lint/golint
+go get -u github.com/josharian/impl
+go get -u github.com/jstemmer/gotags
+go get -u github.com/kisielk/errcheck
+go get -u github.com/klauspost/asmfmt/cmd/asmfmt
+go get -u github.com/koron/iferr
 go get -u github.com/nsf/gocode
+go get -u github.com/rogpeppe/godef
+go get -u github.com/vektra/mockery/...
+go get -u github.com/zmb3/gogetdoc
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/gorename
+go get -u golang.org/x/tools/cmd/guru
+go get -u honnef.co/go/tools/cmd/megacheck
