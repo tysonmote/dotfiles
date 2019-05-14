bde7772b77dabfeb46d2e80f980ef1387809b054
Tyson Mote <tyson@segment.com>
2019-05-14 10:48:00 -0700

    Misc updates

--- go/update-linters.sh
+++ go/update-linters.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -eux
 
-go get -u github.com/mgechev/revive
 go get -u github.com/klauspost/asmfmt/cmd/asmfmt
+go get -u github.com/mgechev/revive
 go get -u github.com/vektra/mockery/...
+go get -u golang.org/x/tools/cmd/gopls
 go get -u honnef.co/go/tools/cmd/megacheck
 
 nvim +GoUpdateBinaries +qall
