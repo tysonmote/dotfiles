5c844f53e9d8265355a96ac19e43f2858489df4a
Tyson Mote <tyson@segment.com>
2019-05-14 10:55:34 -0700

    go: Add GO111MODULE=on to update script

--- go/update-linters.sh
+++ go/update-linters.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eux
 
+export GO111MODULE=on
+
 go get -u github.com/klauspost/asmfmt/cmd/asmfmt
 go get -u github.com/mgechev/revive
-go get -u github.com/vektra/mockery/...
+go get -u github.com/vektra/mockery
 go get -u golang.org/x/tools/cmd/gopls
 go get -u honnef.co/go/tools/cmd/megacheck
 
