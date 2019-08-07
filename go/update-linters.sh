7118d912873f5ff83913ad6ded42f93e74b930c8
Tyson Mote <tyson@segment.com>
2019-08-07 15:44:57 -0700

    go: Update linter update script

--- go/update-linters.sh
+++ go/update-linters.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
-export GO111MODULE=on
+export GO111MODULE=off
 
 go get -u github.com/klauspost/asmfmt/cmd/asmfmt
 go get -u github.com/mgechev/revive
 go get -u github.com/vektra/mockery
 go get -u golang.org/x/tools/cmd/gopls
-go get -u honnef.co/go/tools/cmd/megacheck
+go get -u honnef.co/go/tools/...
 
 nvim +GoUpdateBinaries +qall
 
