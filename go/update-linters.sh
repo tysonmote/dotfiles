edfa77f9e57e362fe15ece994c9eec6792f0d9ad
Tyson Mote <tyson@bufio.net>
2021-10-25 20:49:42 -0700

    go: Update tools

--- go/update-linters.sh
+++ go/update-linters.sh
@@ -2,11 +2,11 @@
 
 export GO111MODULE=on
 
-go get github.com/klauspost/asmfmt/cmd/asmfmt@latest
-go get github.com/mgechev/revive@latest
-go get github.com/vektra/mockery@latest
-go get golang.org/x/tools/gopls@latest
-go get honnef.co/go/tools/...@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/klauspost/asmfmt/cmd/asmfmt@latest
+go install github.com/mgechev/revive@latest
+go install golang.org/x/tools/gopls@latest
+go install honnef.co/go/tools/...@latest
 
 nvim +GoUpdateBinaries +qall
 
